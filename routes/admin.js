376f7718320252d84dda598ab2c7552216482976
Kbikash24 <bikashk11@gmail.com>
2024-05-08 17:05:24 +0530

    task

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,10 @@
+const path=require('path')
 const express=require('express');
+const routeDir=require('../utils/path')
 
  const router=express.Router();
  router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(routeDir,'view','add-product.html'))
   });
   
   router.post('/product', (req, res, next) => {
--- routes/contact.js
+++ routes/contact.js
@@ -4,8 +4,8 @@ const routeDir=require('../utils/path')
 const router=express.Router();
 
 
-router.get('/', (req, res, next) => {
-    res.sendFile(path.join(routeDir,'view','shop.html'))
+router.get('/contact', (req, res, next) => {
+    res.sendFile(path.join(routeDir,'view','contact.html'))
   });
 
   module.exports=router;
